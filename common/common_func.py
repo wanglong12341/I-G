28ef21823c48fd656270eea49a765ea4be1df2f2
buxiangjie <bxj3416162@163.com>
2018-10-30 14:10:57 +0800

    接口脚本更新

--- common/common_func.py
+++ common/common_func.py
@@ -2,7 +2,8 @@
 # -*- coding: UTF-8 -*-
 from config.configer import Config
 from log.logger import Logger
-import requests,os
+import pymysql
+import requests
 
 class Common():
 
@@ -11,6 +12,7 @@ class Common():
 		self.logger = Logger(logger="Common").getlog()
 
 	def Response(self,faceaddr,param=None):
+		"""post调用"""
 		faceaddrs = Config().Get_Item("InterFaceAddr",faceaddr)
 		url = self.url + faceaddrs
 		header = {
@@ -26,3 +28,29 @@ class Common():
 			return requests.post(url,data=param)
 		except Exception as e:
 			print(str(e))
+
+	def Get_Password(self,merchant_id):
+		"""获取商户登录密码"""
+		host = Config().Get_Item("Database","host")
+		user = Config().Get_Item("Database","user")
+		password = Config().Get_Item("Database","password")
+		port = Config().Get_Item("Database","port")
+		db = Config().Get_Item("Database","db")
+		self.logger.info("数据库连接地址:%s;用户名:%s;密码:%s;端口号:%s;数据库名称:%s"%(host,user,password,port,db))
+		conn = pymysql.connect(host="192.168.19.219",user=user,password=password,port=4376,db=db,charset='utf8')
+		cur = conn.cursor()
+		sql = "Select withdraw_password from merchant_setting where merchant_id=%s"%merchant_id
+		self.logger.info("获取商户密码sql:%s"%sql)
+		cur.execute(sql)
+		conn.commit()
+		pwd = cur.fetchone()
+		self.logger.info("商户密码为:%s"%pwd)
+		cur.close()
+		conn.close()
+		try:
+			return pwd[0]
+		except Exception as e:
+			self.logger.error("获取失败返回:%s"%e)
+			print(str(e))
+
+
--- test_case/test_Merchant_Account_Controller.py
+++ test_case/test_Merchant_Account_Controller.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 from common.common_func import Common
 from log.logger import Logger
-import unittest,json
+import unittest,json,random
 
 class Merchant_Account_Controller(unittest.TestCase):
 	"""主办方账户集合"""
@@ -40,3 +40,47 @@ class Merchant_Account_Controller(unittest.TestCase):
 		reps = json.loads(rep.text)
 		assert rep.status_code == 200, "接口返回状态正确"
 		self.assertEqual(reps['status'],"1008","测试通过")
+
+	def test_merchantAccountIncome3(self):
+		"""我的收入：不存在的主办方id"""
+		param = {
+			"merchantId": 10115246331
+		}
+		rep = self.cm.Response(faceaddr="merchantAccountIncome", param=param)
+		print("返回信息:%s" % rep.text)
+		print("返回信息头:%s" % rep.headers)
+		self.logger.info("返回信息:%s" % rep.text)
+		self.logger.info("返回信息头:%s" % rep.headers)
+		assert rep.status_code == 400, "接口返回状态正确"
+
+	def test_merchantAccountIncome4(self):
+		"""我的收入：输入非数字类型的主办方ID"""
+		param = {
+			"merchantId": "&@&@*"
+		}
+		rep = self.cm.Response(faceaddr="merchantAccountIncome", param=param)
+		print("返回信息:%s" % rep.text)
+		print("返回信息头:%s" % rep.headers)
+		self.logger.info("返回信息:%s" % rep.text)
+		self.logger.info("返回信息头:%s" % rep.headers)
+		assert rep.status_code == 400, "接口返回状态正确"
+
+	def test_updatePassword(self):
+		"""修改小程序密码：正常传参"""
+		strr = 'a' + ''
+		for y in range(6):
+			strr += (random.choice("0123456789"))
+		merchant_id = 10115246
+		param = {
+			"merchantId": merchant_id,
+			"password": strr,
+		}
+		rep = self.cm.Response(faceaddr="updatePassword", param=param)
+		print("返回信息:%s" % rep.text)
+		print("返回信息头:%s" % rep.headers)
+		self.logger.info("返回信息:%s" % rep.text)
+		self.logger.info("返回信息头:%s" % rep.headers)
+		pwd = Common().Get_Password(merchant_id=str(merchant_id))
+		self.logger.info("修改后的密码为:%s"%pwd)
+		assert rep.status_code == 200, "接口返回状态正确"
+		self.assertEqual(strr,pwd,"账号正确")
