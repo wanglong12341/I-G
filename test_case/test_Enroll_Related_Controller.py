8aa6b56eb476edb417164d360d3fab22f14b43b6
buxiangjie <bxj3416162@163.com>
2018-11-12 15:01:12 +0800

    接口脚本更新

---
+++ test_case/test_Enroll_Related_Controller.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from common.common_func import Common
+from log.logger import Logger
+import unittest,json,time,os
+
+class Enroll_Related_Controller(unittest.TestCase):
+	"""发布活动接口集合"""
+	def setUp(self):
+		self.cm = Common()
+		self.logger = Logger(logger="Enroll_Related_Controller").getlog()
+
+	def tearDown(self):
+		pass
+
+	def test_createAppletOrder1(self):
+		"""创建订单：正常传参"""
+		param = {
+			"activityId": 935,
+			"orderPrice": 0,
+			"payPrice": 0,
+			"ticketCount":1,
+			"memberId":64757,
+			"merchantId":10115246,
+			"contactsName":"卜祥杰",
+			"contactsPhone":"18366582857",
+			"channelId":880,
+			"inventoryId":2435,
+			"ticketId":1310,
+			"enrollInfos":"[{'ticket_id': '1232','enrollXml': '职位c040'}]",
+		}
+		rep = self.cm.Response(faceaddr="createAppletOrder", param=param)
+		print("返回信息:%s" % rep.text)
+		print("返回信息头:%s" % rep.headers)
+		self.logger.info("返回信息:%s" % rep.text)
+		self.logger.info("返回信息头:%s" % rep.headers)
+		reps = json.loads(rep.text)
+		self.logger.info("订单号为:%s"%reps['data'])
+		assert rep.status_code == 200, "接口返回状态正确"
---
+++ test_case/test_Ticket_Check_Controller.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from common.common_func import Common
+from log.logger import Logger
+import unittest,json,time,os
+
+class Ticket_Check_Controller(unittest.TestCase):
+	"""发布活动接口集合"""
+	def setUp(self):
+		self.cm = Common()
+		self.logger = Logger(logger="Ticket_Check_Controller").getlog()
+
+	def tearDown(self):
+		pass
+
+	def test_ticketAudit1(self):
+		"""票券审核：拒绝审核不填写原因"""
+		param = {
+			"ids":1310,
+			"userId":270,
+			"status":2,
+		}
+		rep = self.cm.Response(faceaddr="ticketAudit", param=param)
+		print("返回信息:%s" % rep.text)
+		print("返回信息头:%s" % rep.headers)
+		self.logger.info("返回信息:%s" % rep.text)
+		self.logger.info("返回信息头:%s" % rep.headers)
+		reps = json.loads(rep.text)
+		assert rep.status_code == 200, "接口返回状态正确"
+		self.assertEqual(reps['data'],"1022,拒绝时必须填原因","测试通过")
+
+	def test_ticketAudit2(self):
+		"""票券审核：拒绝审核填写原因"""
+		param = {
+			"ids":1310,
+			"userId":270,
+			"status":2,
+			"reason":"我舅舅家",
+		}
+		rep = self.cm.Response(faceaddr="ticketAudit", param=param)
+		print("返回信息:%s" % rep.text)
+		print("返回信息头:%s" % rep.headers)
+		self.logger.info("返回信息:%s" % rep.text)
+		self.logger.info("返回信息头:%s" % rep.headers)
+		assert rep.status_code == 200, "接口返回状态正确"
+
+	def test_ticketCheckList1(self):
+		"""票券审核列表：正常传参"""
+		param = {
+			"page":1,
+			"size":1,
+			"userId":270,
+			"activityId":935,
+		}
+		rep = self.cm.Response(faceaddr="ticketCheckList", param=param)
+		print("返回信息:%s" % rep.text)
+		print("返回信息头:%s" % rep.headers)
+		self.logger.info("返回信息:%s" % rep.text)
+		self.logger.info("返回信息头:%s" % rep.headers)
+		assert rep.status_code == 200, "接口返回状态正确"
+
+if __name__ == '__main__':
+	unittest.main()
