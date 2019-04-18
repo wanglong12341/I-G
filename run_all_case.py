c7d919f3141a0da0693ef0dd15aa56cc24d2fc86
buxiangjie <bxj3416162@163.com>
2019-04-18 15:33:29 +0800

    增加外部参数3  环境参数

--- run_all_case.py
+++ run_all_case.py
@@ -13,11 +13,12 @@ from config.configer import Config
 sys.path.append('')
 logger = Logger(logger="run_all_case").getlog()
 
+
 def all_case(dir):
 	'''批量添加用例到测试套件'''
 	if dir == 'chezhibao':
 		case_dir = "./chezhibao"
-		logger.info("用例执行文件夹%s"%case_dir)
+		logger.info("用例执行文件夹%s" % case_dir)
 	elif dir == 'czb_tp':
 		case_dir = "./czb_tp"
 		logger.info("用例执行文件夹%s" % case_dir)
@@ -46,7 +47,7 @@ def sendreport(file_new):
 	'''使用smtp发送测试报告'''
 	with open(file_new, 'rb') as f:
 		mail_body = f.read()
-	logger.info("mail_body:%s"%mail_body)
+	logger.info("mail_body:%s" % mail_body)
 	msg = MIMEMultipart()
 	msg['From'] = 'bxj3416162@163.com'
 	msg['To'] = 'buxiangjie@cloudloan.com'
@@ -69,7 +70,7 @@ def sendreport(file_new):
 		msg['Subject'] = Header('中投保报告', 'utf8')
 		msg['CC'] = 'wangxl@cloudloan.com'
 
-	msg_file = MIMEText(mail_body,'base64','utf8')
+	msg_file = MIMEText(mail_body, 'base64', 'utf8')
 	msg_file['Content-Type'] = 'application/octet-stream'
 	msg_file["Content-Disposition"] = 'attachment;filename=report.html'
 	msg.attach(msg_file)
@@ -90,10 +91,11 @@ def newreport(test_report):
 	file_new = os.path.join(test_report, lists2[-1])  # 找到正序排序的下面一个文件，即最新的文件
 	return file_new
 
+
 def set_driver(system):
 	if system == 'mac':
-		Config().Set_Item('Driver','system',system)
-		logger.info("driver:%s设置成功"%system)
+		Config().Set_Item('Driver', 'system', system)
+		logger.info("driver:%s设置成功" % system)
 	elif system == 'linux':
 		Config().Set_Item('Driver', 'system', system)
 		logger.info("driver:%s设置成功" % system)
@@ -102,6 +104,7 @@ def set_driver(system):
 		logger.info("driver:%s设置成功" % system)
 	else:
 		raise Exception("不支持该系统设置driver")
+
 
 if __name__ == "__main__":
 	# dir = input("dir:")
@@ -114,4 +117,4 @@ if __name__ == "__main__":
 	runner.run(all_case(sys.argv[1]))
 	fp.close()
 	new_report = newreport(os.getcwd() + "/test_report")
-	sendreport(new_report)#调用发送报告函数
+	sendreport(new_report)  # 调用发送报告函数
